c54122f247d4a36fd77704d70e8ebaa3349adb4d
estebanrfp <estebanrfp@gmail.com>
2019-07-31 13:22:39 +0200

    added exceptions to snapshot example

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,6 +5,21 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+    test('Siempre fallará la instantanea', () => {
+        const user = {
+            createAt: new Date,
+            id: Math.floor(Math.random() * 20)
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepsión dentro del código', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Esteban Fuster Pozzi"
+        }
+        expect(user).toMatchSnapshot({ id: expect.any(Number) });
+    });
 });
 
 // jest -u (update snapshots)
